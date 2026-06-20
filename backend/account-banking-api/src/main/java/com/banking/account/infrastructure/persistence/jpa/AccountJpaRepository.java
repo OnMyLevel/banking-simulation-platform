a4587139710fc99461789ae174e4b3bcc949f3c7
OnMyLevel <merilb78@gmail.com>
2026-06-20 19:04:13 +0200

    feat(account): add Spring Data repository

---
+++ backend/account-banking-api/src/main/java/com/banking/account/infrastructure/persistence/jpa/AccountJpaRepository.java
@@ -0,0 +1,9 @@
+package com.banking.account.infrastructure.persistence.jpa;
+
+import com.banking.account.infrastructure.persistence.entity.AccountEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface AccountJpaRepository extends JpaRepository<AccountEntity, UUID> {
+}
