7780a04661a2bd18ea598cc4ded7bcc04b135eb8
OnMyLevel <merilb78@gmail.com>
2026-06-20 19:26:23 +0200

    feat(account): add owner account JPA query

--- backend/account-banking-api/src/main/java/com/banking/account/infrastructure/persistence/jpa/AccountJpaRepository.java
+++ backend/account-banking-api/src/main/java/com/banking/account/infrastructure/persistence/jpa/AccountJpaRepository.java
@@ -1,9 +1,12 @@
 package com.banking.account.infrastructure.persistence.jpa;
 
 import com.banking.account.infrastructure.persistence.entity.AccountEntity;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface AccountJpaRepository extends JpaRepository<AccountEntity, UUID> {
+    List<AccountEntity> findByOwnerId(UUID ownerId, Pageable pageable);
 }
