6b7388df2a8e47319ac65560b019e211b331c1e5
OnMyLevel <merilb78@gmail.com>
2026-06-20 18:41:38 +0200

    feat(user): add user JPA adapter

---
+++ backend/user-management-api/src/main/java/com/banking/user/infrastructure/persistence/adapter/UserJpaAdapter.java
@@ -0,0 +1,29 @@
+package com.banking.user.infrastructure.persistence.adapter;
+
+import com.banking.user.domain.model.User;
+import com.banking.user.domain.repository.UserRepository;
+import com.banking.user.infrastructure.persistence.jpa.UserJpaRepository;
+import com.banking.user.infrastructure.persistence.mapper.UserMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public class UserJpaAdapter implements UserRepository {
+    private final UserJpaRepository userJpaRepository;
+
+    public UserJpaAdapter(UserJpaRepository userJpaRepository) {
+        this.userJpaRepository = userJpaRepository;
+    }
+
+    @Override
+    public User save(User user) {
+        return UserMapper.toDomain(userJpaRepository.save(UserMapper.toEntity(user)));
+    }
+
+    @Override
+    public Optional<User> findById(UUID id) {
+        return userJpaRepository.findById(id).map(UserMapper::toDomain);
+    }
+}
