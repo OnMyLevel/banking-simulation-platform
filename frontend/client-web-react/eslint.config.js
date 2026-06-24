f598338b93297e6b70f4579e2be26d5f22f389ce
OnMyLevel <merilb78@gmail.com>
2026-06-24 19:12:28 +0200

    feat client web setup (#49)
    * feat client web add package setup
    
    * feat client web add tsconfig
    
    * feat client web add node tsconfig
    
    * feat client web add vite config
    
    * test client web add vitest config
    
    * chore client web add eslint config
    
    * feat client web add index html
    
    * feat client web add react entrypoint
    
    * feat client web add placeholder app
    
    * feat client web add base styles
    
    * docs client web update setup commands
    
    * docs add PR 49 changelog

---
+++ frontend/client-web-react/eslint.config.js
@@ -0,0 +1,30 @@
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+
+export default [
+  {
+    ignores: ['dist'],
+  },
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    },
+  },
+];
