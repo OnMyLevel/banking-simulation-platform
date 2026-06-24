e157ad6447aedceaf33303feea92971786276ca0
OnMyLevel <merilb78@gmail.com>
2026-06-24 17:03:53 +0200

    feat gateway add traffic properties

---
+++ backend/api-gateway/src/main/java/com/banking/gateway/traffic/GatewayTrafficProperties.java
@@ -0,0 +1,43 @@
+package com.banking.gateway.traffic;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "banking.gateway")
+public class GatewayTrafficProperties {
+    private int usersBudgetPerMinute = 120;
+    private int accountsBudgetPerMinute = 60;
+    private int operationsBudgetPerMinute = 30;
+    private int defaultBudgetPerMinute = 180;
+
+    public int usersBudgetPerMinute() {
+        return usersBudgetPerMinute;
+    }
+
+    public void setUsersBudgetPerMinute(int usersBudgetPerMinute) {
+        this.usersBudgetPerMinute = usersBudgetPerMinute;
+    }
+
+    public int accountsBudgetPerMinute() {
+        return accountsBudgetPerMinute;
+    }
+
+    public void setAccountsBudgetPerMinute(int accountsBudgetPerMinute) {
+        this.accountsBudgetPerMinute = accountsBudgetPerMinute;
+    }
+
+    public int operationsBudgetPerMinute() {
+        return operationsBudgetPerMinute;
+    }
+
+    public void setOperationsBudgetPerMinute(int operationsBudgetPerMinute) {
+        this.operationsBudgetPerMinute = operationsBudgetPerMinute;
+    }
+
+    public int defaultBudgetPerMinute() {
+        return defaultBudgetPerMinute;
+    }
+
+    public void setDefaultBudgetPerMinute(int defaultBudgetPerMinute) {
+        this.defaultBudgetPerMinute = defaultBudgetPerMinute;
+    }
+}
