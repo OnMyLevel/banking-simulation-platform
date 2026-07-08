08ff31859a9684835bc55946ebb84f37b6a7ac44
OnMyLevel <merilb78@gmail.com>
2026-07-08 14:45:29 +0200

    feat provide angular http client

--- frontend/angular-app/src/main.ts
+++ frontend/angular-app/src/main.ts
@@ -1,4 +1,7 @@
+import { provideHttpClient } from '@angular/common/http';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent).catch((error: unknown) => console.error(error));
+bootstrapApplication(AppComponent, {
+  providers: [provideHttpClient()],
+}).catch((error: unknown) => console.error(error));
