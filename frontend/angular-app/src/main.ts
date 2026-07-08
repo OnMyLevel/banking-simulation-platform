72d18c59c7669bc21c6a7918241d17234143d88a
OnMyLevel <merilb78@gmail.com>
2026-07-08 15:13:38 +0200

    feat register correlation id interceptor

--- frontend/angular-app/src/main.ts
+++ frontend/angular-app/src/main.ts
@@ -1,7 +1,8 @@
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
+import { correlationIdInterceptor } from './app/correlation-id.interceptor';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideHttpClient()],
+  providers: [provideHttpClient(withInterceptors([correlationIdInterceptor]))],
 }).catch((error: unknown) => console.error(error));
